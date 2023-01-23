fb8c56e5d16fb33a3a054c4b320c2bcc9fa6e8e4
shahriar <you@example.com>
2023-01-23 12:26:53 +0330

    the heder first time done

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Ali from './in-head/UpMenu'
+import UpMenu from './in-head/UpMenu'
 
 
 export default function Header() {
   return (
       <div>
-        <Ali></Ali>
+        <UpMenu></UpMenu>
       </div>
   )
 }
--- src/Header/in-head/UpMenu.js
+++ src/Header/in-head/UpMenu.js
@@ -1,55 +1,69 @@
 
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import './UpMenu.css'
 
 export default function UpMenu() {
   return (
-    
-      <div class="container-fluid up-menu">
-      <div class="container">
-          <div class="row justify-content-center justify-content-lg-between py-2">
-              <div class="col-auto text-center">
-                  <ul class=" list-inline mb-0 contact-box ">
-                      <li class=" list-inline-item en-text d-block d-lg-inline-block">
-                          <i class="fa-solid fa-phone"></i>
-                          013-44444444
-
-                      </li>
-                      <li class="list-inline-item en-text d-block d-lg-inline-block">
-                          <i class="fa-solid fa-envelope"></i>
-                          This.Email.gmail.com
-                      </li>
-                  </ul>
-              </div>
-              <div class="w-100 d-lg-none"></div>
-              <div class="col-auto mt-3 mt-lg-0 text-center">
-                  <div class="social-box  d-inline-block me-sm-3 pe-sm-3">
-                      <span>
-                          دنبال کنید
-                      </span>
-                      <ul class="list-inline d-inline-block mb-0">
-                          <li class="list-inline-item">
-                              <i class="fab fa-facebook-f"></i>
-                          </li>
-                          <li class="list-inline-item">
-                              <i class="fab fa-twitter"></i>
-                          </li>
-                          <li class="list-inline-item">
-                              <i class="fab fa-instagram"></i>
-                          </li>
-                          <li class="list-inline-item">
-                              <i class="fab fa-pinterest-p"></i>
-                          </li>
-                      </ul>
-                  </div>
-                  <div class="search-box d-inline-block position-relative mt-2 mt-sm-0 ">
-                      <i class="fas fa-search"></i>
-                      <input type="text" placeholder="جستجو..."/>
-                  </div>
-              </div>
-          </div>
+    <Container className='Up-Menu'>
+        <Row>
+        <Col>
+        <div className='addres-box' >
+           <ul class="col list-inline mb-0 contact-box ">
+               <li class="list-inline-item entext d-block d-lg-inline-block">
+                   <i class="fas fa-phone"></i>
+                   021-88888888
+               </li>
+               <li class="list-inline-item entext d-block d-lg-inline-block">
+                   <i class="far fa-envelope"></i>
+                   websila.ir@gmail.com
+               </li>
+               <li class="list-inline-item d-none d-lg-inline-block">
+                   <i class="far fa-envelope"></i>
+                   تهران
+               </li>
+           </ul>
+         </div>
+        </Col>
+        <Col>
+       <div class="col mt-3 mt-lg-0 text-center">
+        <div class="social-box d-inline-block me-sm-3 pe-sm-3">
+          <span>دنبال کنید :</span>
+          <ul class="list-inline d-inline-block mb-0">
+            <li class="list-inline-item">
+              <i class="fab fa-facebook-f"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="fab fa-twitter"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="fab fa-instagram"></i>
+            </li>
+            <li class="list-inline-item">
+              <i class="fab fa-pinterest-p"></i>
+            </li>
+          </ul>
+        </div>
+        
+             
+  
+     </div>
+    </Col>
+    <Col>
+      <div
+        class="search-box d-inline-block position-relative mt-2 mt-sm-0"
+      >
+        <i class="fas fa-search"></i>
+        <input type="text" placeholder="جستجو..." />
       </div>
-    </div>
+    </Col>
+      </Row>
+    </Container>
+    
    
   )
 }
+
